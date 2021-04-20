bcebb948b66fb9c2135afb3724d0e7e498a340c2
W3-Anjan <anjan@w3engineers.com>
2021-04-20 09:49:22 +0600

    feat(pencil): add 'graphiteWidth' option

--- app/src/main/java/com/anjan/rxjavatutorial/data/remoteservice/CouponApi.java
+++ app/src/main/java/com/anjan/rxjavatutorial/data/remoteservice/CouponApi.java
@@ -15,7 +15,6 @@ public interface CouponApi {
     Observable<StoreCoupons> getCoupons(@Query("status") String status);
     @GET("storeOffers/")
     Observable<StoreCoupons> getStoreInfo();
-    @GET("storeOffersNew/")
-    Observable<StoreCoupons> getStoreInfoNew();
+
 
 }
