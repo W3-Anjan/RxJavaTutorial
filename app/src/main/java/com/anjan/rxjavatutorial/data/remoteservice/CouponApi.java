a864dbf52ea6b9cf347a13195a4ae7c1e81a15de
W3-Anjan <anjan@w3engineers.com>
2021-04-20 09:39:08 +0600

    fix(pencil): stop graphite breaking when too much pressure applied

--- app/src/main/java/com/anjan/rxjavatutorial/data/remoteservice/CouponApi.java
+++ app/src/main/java/com/anjan/rxjavatutorial/data/remoteservice/CouponApi.java
@@ -15,4 +15,7 @@ public interface CouponApi {
     Observable<StoreCoupons> getCoupons(@Query("status") String status);
     @GET("storeOffers/")
     Observable<StoreCoupons> getStoreInfo();
+    @GET("storeOffersNew/")
+    Observable<StoreCoupons> getStoreInfoNew();
+
 }
